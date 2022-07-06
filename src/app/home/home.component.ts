03fd5ea3efa7e430d0adabeece3c3837f8c8c58b
rupikaur123 <priyadanu99@gmail.com>
2022-07-06 17:18:45 +0530

    api_implementation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -118,6 +118,7 @@ export class HomeComponent implements OnInit {
     }
   ]
   res:any
+  treatment_name:any=[]
   constructor(config: NgbCarouselConfig, private route: Router,public http: HttpClient) {
     console.log('images', this.images);
     // customize default values of carousels used by this component tree
@@ -128,6 +129,26 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getReviewList()
+    this.getServiceList()
+  }
+  getServiceList() {
+    this.http.get<any>(this.baseUrl + 'api/get_services').subscribe({
+      next: (data: any) => {
+        console.log('Get completed sucessfully. The response received ' + data);
+        this.res = data.data;
+        this.treatment_name = this.res;
+        console.log('treatment_name', this.treatment_name);
+      },
+      error: (err: any) => {
+        console.log('failed with the errors', err.error);
+        if (err.error) {
+          // this.toster.error(err.error.message);
+        } else {
+          // this.toster.error('Something went wrong');
+        }
+      },
+      complete: () => {},
+    });
   }
   getReviewList(){
     this.http.get<any>(this.baseUrl + 'api/reviews_front').subscribe({
